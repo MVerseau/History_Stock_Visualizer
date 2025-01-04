ef4585a78e7e148b2f1a7eabd18f17f6ed499786
Mverseau <mverseau@gmail.com>
2025-01-04 18:18:08 +0300

    export to .csv file added.

--- data_download.py
+++ data_download.py
@@ -12,12 +12,17 @@ def add_moving_average(data, window_size=5):
     return data
 
 
+def export_data_to_csv(data, filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        data.to_csv(f, sep=',')
+    print(f"Данные сохранены в {filename}")
+
+
 def calculate_and_display_average_price(data):
     print(f'Среднее значение цены закрытия за период: {data['Close'].mean():.6f}')
 
 
 def notify_if_strong_fluctuations(data, fluctuation: float):
     result = data['Close'].agg(['min', 'max']).pct_change()
-    if result.iloc[-1]*100 > fluctuation:
-        print(f"\033[91mВНИМАНИЕ: Высокая волатильность ({result.iloc[-1]*100:.4f}% за период)!!! \033[00m")
-
+    if result.iloc[-1] * 100 > fluctuation:
+        print(f"\033[91mВНИМАНИЕ: Высокая волатильность ({result.iloc[-1] * 100:.4f}% за период)!!! \033[00m")
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import data_download as dd
 import data_plotting as dplt
+from datetime import date
 
 
 def main():
     print("Добро пожаловать в инструмент получения и построения графиков биржевых данных.")
-    print("Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
-    print("Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
+    print(
+        "Вот несколько примеров биржевых тикеров, которые вы можете рассмотреть: AAPL (Apple Inc), GOOGL (Alphabet Inc), MSFT (Microsoft Corporation), AMZN (Amazon.com Inc), TSLA (Tesla Inc).")
+    print(
+        "Общие периоды времени для данных о запасах включают: 1д, 5д, 1мес, 3мес, 6мес, 1г, 2г, 5г, 10л, с начала года, макс.")
 
     ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):»")
     period = input("Введите период для данных (например, '1mo' для одного месяца): ")
-    fluctuation=float(input('Введите процент изменения цены для определения высокой волатильности: '))
+    fluctuation = float(input('Введите процент изменения цены для определения высокой волатильности: '))
+    export = input('Нужно ли данные сохранить в файл? (д/н) ')
 
     # Fetch stock data
     stock_data = dd.fetch_stock_data(ticker, period)
@@ -22,6 +26,11 @@ def main():
 
     # Detecting and alerting high fluctuation being more than number of per cent set by the user (10 % by default) price change
     dd.notify_if_strong_fluctuations(stock_data, fluctuation)
+
+    # Export data to .csv file
+    if export.lower() in 'lд':
+        filename = f'{ticker}_{period}_stock_price_{date.today()}.csv'.replace('-','_')
+        dd.export_data_to_csv(stock_data, filename)
 
     # Plot the data
     dplt.create_and_save_plot(stock_data, ticker, period)
