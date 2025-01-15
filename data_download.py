e49cbcc7e61a511ceadb27bbe0735a1a5f40cf26
Марина <168333708+MVerseau@users.noreply.github.com>
2025-01-15 15:13:11 +0300

    Возможность указывать период

--- data_download.py
+++ data_download.py
@@ -1,5 +1,23 @@
 import yfinance as yf
 
+def set_period(period):
+    if set(period).intersection({".",'/','-'}):
+        get_period(period)
+    return period
+
+def get_period(period, count=0):
+    
+    start=datetime.strptime(period.replace('-','/').replace('.','/').strip('"'),'%d/%m/%Y')
+    #start=datetime.strftime(start, '%Y-%m-%d')
+    end=input('Введите дату окончания периода ("дд.мм.гггг"): ')
+    end=datetime.strptime(end.replace('-','/').replace('.','/').strip('\"'),'%d/%m/%Y')
+    if end>start: # TODO: Предусмотреть ошибку при многократном вводе некорретной даты.
+        count+=1
+        print("Дата окончания периода раньше даты начала периода.")
+        get_period(period)
+    period=dict(start=start, end=end) #TODO: Проверить, что примет в рассчет stock.history
+    #end=datetime.strftime(end, '%Y-%m-%d') 
+    return period
 
 def fetch_stock_data(ticker, period='1mo'):
     stock = yf.Ticker(ticker)
